a9772c01f4458681771e00e35d3cade91658b969
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2025-07-02 17:21:16 +0200

    Remove unnecessary Zone.Identifier files from various project directories

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Mikkel Kornval',
+  description: 'Resume of Mikkel Kornval',
 }
 
 export default function RootLayout({
